eab2d70fb95141d30cec1800a808671dc56e2d80
Elen Oliveira <elenoliveirafernandes@gmail.com>
2025-09-27 12:36:24 +0100

    promises/asyncAwait

--- exercicios_js/esnext/promise2.js
+++ exercicios_js/esnext/promise2.js
@@ -15,8 +15,8 @@
 function esperarPor(tempo = 2000){
     return new Promise(function(resolve){
         setTimeout(function(){
-        console.log('Executando promise...')
-        resolve()
+            console.log('Executando promise...')
+            resolve()
         }, tempo)
     })
 }
